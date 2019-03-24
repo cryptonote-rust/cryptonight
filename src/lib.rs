567ca6ff346a896cc99eb5cf6ff0ae718d8f2996
calidion <calidion@gmail.com>
2019-03-25 00:33:05 +0800

    removed none cryptonight code

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,4 @@ extern crate log;
 
 pub mod byte_string;
 pub mod cryptonight;
-pub mod stratum;
-pub mod worker;
 pub mod u64x2;
-pub mod metric;
-pub mod bandit_tools;
-pub mod mithril_config;
-pub mod timer;
